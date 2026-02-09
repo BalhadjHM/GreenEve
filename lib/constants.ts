8540a1ccaaced1c3607a172e894444e00a0996b7
BalhadjHM <elhadjmoussa.balhadj@univ-guelma.dz>
2026-02-09 22:34:32 +0800

    first commit

---
+++ lib/constants.ts
@@ -0,0 +1,155 @@
+export interface Event {
+  slug: string;
+  title: string;
+  image: string;
+  location?: string;
+  date?: string;
+  time?: string;
+}
+
+export const events: Event[] = [
+  {
+    slug: "devfest-algiers-2026",
+    title: "DevFest Algiers 2026",
+    image: "/images/event1.png",
+    location: "Palais des Expositions, Algiers",
+    date: "March 15, 2026",
+    time: "9:00 AM - 6:00 PM"
+  },
+  {
+    slug: "algeria-healthcare-summit",
+    title: "Algeria Healthcare Summit 2026",
+    image: "/images/event2.png",
+    location: "Sheraton Hotel, Oran",
+    date: "April 22, 2026",
+    time: "8:30 AM - 5:00 PM"
+  },
+  {
+    slug: "mediterranean-cultural-festival",
+    title: "Mediterranean Cultural Festival",
+    image: "/images/event3.png",
+    location: "Theatre National Algerien, Algiers",
+    date: "May 10-12, 2026",
+    time: "All Day Event"
+  },
+  {
+    slug: "algeria-renewable-energy-expo",
+    title: "Algeria Renewable Energy Expo",
+    image: "/images/event4.png",
+    location: "SAFEX Exhibition Centre, Algiers",
+    date: "March 28, 2026",
+    time: "10:00 AM - 7:00 PM"
+  },
+  {
+    slug: "startup-weekend-constantine",
+    title: "Startup Weekend Constantine",
+    image: "/images/event5.png",
+    location: "University of Constantine 2",
+    date: "February 28 - March 2, 2026",
+    time: "Friday 6:00 PM - Sunday 8:00 PM"
+  },
+  {
+    slug: "algiers-international-book-fair",
+    title: "Algiers International Book Fair",
+    image: "/images/event6.png",
+    location: "Palais des Expositions, Pins Maritimes",
+    date: "June 5-15, 2026",
+    time: "9:00 AM - 9:00 PM"
+  },
+  {
+    slug: "algeria-business-leaders-forum",
+    title: "Algeria Business Leaders Forum",
+    image: "/images/event7.png",
+    location: "Hilton Algiers",
+    date: "April 8, 2026",
+    time: "9:00 AM - 4:00 PM"
+  },
+  {
+    slug: "sahara-marathon-annaba",
+    title: "Sahara Marathon Annaba 2026",
+    image: "/images/event8.png",
+    location: "Annaba Coastal Route",
+    date: "March 21, 2026",
+    time: "7:00 AM Start"
+  },
+  {
+    slug: "algeria-fashion-week",
+    title: "Algeria Fashion Week 2026",
+    image: "/images/event9.png",
+    location: "Palais de la Culture, Algiers",
+    date: "May 18-22, 2026",
+    time: "6:00 PM - 11:00 PM"
+  },
+  {
+    slug: "maghreb-startup-summit",
+    title: "Maghreb Startup Summit",
+    image: "/images/event10.png",
+    location: "Hotel El Aurassi, Algiers",
+    date: "April 15, 2026",
+    time: "8:00 AM - 6:00 PM"
+  },
+  {
+    slug: "algeria-agriculture-innovation",
+    title: "Algeria Agriculture & Innovation Forum",
+    image: "/images/event11.png",
+    location: "SAFEX, Algiers",
+    date: "March 5, 2026",
+    time: "9:00 AM - 5:00 PM"
+  },
+  {
+    slug: "oran-film-festival",
+    title: "Oran Arab Film Festival",
+    image: "/images/event12.png",
+    location: "Cinema Maghreb, Oran",
+    date: "June 20-27, 2026",
+    time: "Multiple Screenings Daily"
+  },
+  {
+    slug: "women-in-tech-algeria",
+    title: "Women in Tech Algeria Conference",
+    image: "/images/event13.png",
+    location: "Startup House, Algiers",
+    date: "March 8, 2026",
+    time: "10:00 AM - 6:00 PM"
+  },
+  {
+    slug: "algeria-tourism-expo",
+    title: "Algeria Tourism & Hospitality Expo",
+    image: "/images/event14.png",
+    location: "SAFEX Exhibition Centre, Algiers",
+    date: "April 30, 2026",
+    time: "10:00 AM - 8:00 PM"
+  },
+  {
+    slug: "environmental-sustainability-summit",
+    title: "Environmental Sustainability Summit Algeria",
+    image: "/images/event15.png",
+    location: "Es Senia Convention Centre, Oran",
+    date: "May 5, 2026",
+    time: "9:00 AM - 5:30 PM"
+  },
+  {
+    slug: "algeria-fintech-conference",
+    title: "Algeria FinTech & Banking Conference",
+    image: "/images/event16.png",
+    location: "Sofitel Algiers",
+    date: "March 25, 2026",
+    time: "8:30 AM - 4:30 PM"
+  },
+  {
+    slug: "tlemcen-music-festival",
+    title: "Tlemcen International Music Festival",
+    image: "/images/event17.png",
+    location: "Cultural Palace of Tlemcen",
+    date: "July 10-17, 2026",
+    time: "Evening Performances"
+  },
+  {
+    slug: "blockchain-web3-hackathon",
+    title: "Blockchain & Web3 Hackathon Algeria",
+    image: "/images/event18.png",
+    location: "ESI Algiers",
+    date: "May 15-17, 2026",
+    time: "48-Hour Event"
+  }
+];
