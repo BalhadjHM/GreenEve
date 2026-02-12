8f130eff4d83c0729f136cc6966e8de5f30bd9c9
BalhadjHM <elhadjmoussa.balhadj@univ-guelma.dz>
2026-02-12 08:12:13 +0800

    Implement Posthog

--- app/providers.tsx
+++ app/providers.tsx
@@ -5,14 +5,24 @@ import { useEffect } from "react";
 
 import posthog from "posthog-js";
 import { PostHogProvider as PHProvider } from "posthog-js/react";
+import PostHogPageView from "./PostHogPageView"; 
+
+// 1. Initialize PostHog outside the component
+if (typeof window !== "undefined") {
+	posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+		api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+		person_profiles: "identified_only",
+		capture_pageview: false, 
+		autocapture: true, 
+		defaults: "2025-11-30",
+	});
+}
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
-	useEffect(() => {
-		posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-			api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-			defaults: "2025-11-30",
-		});
-	}, []);
-
-	return <PHProvider client={posthog}>{children}</PHProvider>;
+	return (
+		<PHProvider client={posthog}>
+			<PostHogPageView />
+			{children}
+		</PHProvider>
+	);
 }
